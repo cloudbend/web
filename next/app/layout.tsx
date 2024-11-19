0e598a74ee774a0c664c753349a4dde15ada74fb
Josh Wiley <josh@cloudbend.dev>
2024-11-19 06:29:06 -0800

    feat(next): move app code, fonts, favicon

---
+++ next/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Noto_Sans as NotoSans, Noto_Sans_Mono as NotoSansMono } from "next/font/google";
+import "./globals.css";
+
+const notoSans = NotoSans();
+const notoSansMono = NotoSansMono();
+
+export const metadata: Metadata = {
+  title: "Cloudbend",
+  description: "Bend the cloud to your will",
+};
+
+const Layout = ({ children }: { children: React.ReactNode }) => (
+  <html lang="en">
+    <body
+      className={`${notoSans.className} ${notoSansMono.className} antialiased`}
+    >
+      {children}
+    </body>
+  </html>
+);
+
+export default Layout;
